4564d5bbfa521d0fe54a34a1f202c3d643c673b3
Corey Schultz <Corey.w.schultz@gmail.com>
2017-05-12 21:04:30 -0500

    I added my Demo file!

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO Burgers (id, name, devoured, date)
+
+VALUES
+	(1, 'Veggie Burger', 0, '2017-05-12 18:54:22'),
+	(2, 'Cheese Burger', 1, '2017-05-12 18:56:15'),
+	(3, 'Butter Burger', 0, '2017-05-12 18:58:12'),
